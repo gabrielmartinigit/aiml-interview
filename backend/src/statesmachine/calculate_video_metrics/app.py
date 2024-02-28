9f1acbb1a12ed8517fd730d96ab2e43e83c1720f
Gabriel Bella Martini <gabrielb.martini@gmail.com>
2024-02-28 16:18:36 -0300

    feat: detect objects

--- backend/src/statesmachine/calculate_video_metrics/app.py
+++ backend/src/statesmachine/calculate_video_metrics/app.py
@@ -1,38 +1,69 @@
-# import boto3
-# import json
+import boto3
+from PIL import Image
+from io import BytesIO
+from moviepy.editor import VideoFileClip
 
-# def detect_faces(photo, bucket, region):
-    
-#     session = boto3.Session(profile_name='profile-name',
-#                             region_name=region)
-#     client = session.client('rekognition', region_name=region)
+rekognition = boto3.client("rekognition")
+NOT_ALLOWED = ["Glasses", "Hat", "Cap"]
 
-#     response = client.detect_faces(Image={'S3Object':{'Bucket':bucket,'Name':photo}},
-#                                    Attributes=['ALL'])
 
-#     print('Detected faces for ' + photo)
-#     for faceDetail in response['FaceDetails']:
-#         print('The detected face is between ' + str(faceDetail['AgeRange']['Low'])
-#               + ' and ' + str(faceDetail['AgeRange']['High']) + ' years old')
+def identify_objects(frames_list, objects_list):
+    for frame in frames_list:
+        # Convert frame to bytes
+        frame_bytes = frame_to_bytes(frame)
 
-#         print('Here are the other attributes:')
-#         print(json.dumps(faceDetail, indent=4, sort_keys=True))
+        # Detect objects in the frame
+        response = rekognition.detect_labels(Image={"Bytes": frame_bytes})
 
-#         # Access predictions for individual face details and print them
-#         print("Gender: " + str(faceDetail['Gender']))
-#         print("Smile: " + str(faceDetail['Smile']))
-#         print("Eyeglasses: " + str(faceDetail['Eyeglasses']))
-#         print("Face Occluded: " + str(faceDetail['FaceOccluded']))
-#         print("Emotions: " + str(faceDetail['Emotions'][0]))
+        # Extract the identified objects
+        for label in response["Labels"]:
+            if label["Name"] in NOT_ALLOWED:
+                if label["Name"] not in objects_list:
+                    objects_list.append(label["Name"])
 
-#     return len(response['FaceDetails'])
-    
-# def main():
-#     photo='photo'
-#     bucket='bucket'
-#     region='region'
-#     face_count=detect_faces(photo, bucket, region)
-#     print("Faces detected: " + str(face_count))
+    return objects_list
 
-# if __name__ == "__main__":
-#     main()
+
+def frame_to_bytes(frame):
+    # Convert NumPy array to PIL Image
+    pil_image = Image.fromarray(frame)
+
+    # Convert PIL Image to bytes
+    image_stream = BytesIO()
+    pil_image.save(image_stream, format="JPEG")
+    image_bytes = image_stream.getvalue()
+    image_stream.close()
+    return image_bytes
+
+
+def extract_frames(video_path, frames_list):
+    # Load the video clip
+    video_clip = VideoFileClip(video_path)
+
+    # Get the duration of the video
+    duration = video_clip.duration
+
+    # Set the step size to 5 seconds
+    step_size = 5
+
+    # Iterate over each frame and extract frames every 5 seconds
+    for t in range(0, int(duration), step_size):
+        # Get frame at current time
+        frame = video_clip.get_frame(t)
+        frames_list.append(frame)
+
+    # Close the video clip
+    video_clip.close()
+
+    return frames_list
+
+
+if __name__ == "__main__":
+    # Extract frames
+    frames = extract_frames("9574a2ea-e0e0-40fd-86bf-4a37c8a4628d.qt", [])
+
+    # Identify objects
+    objects = identify_objects(frames, [])
+    print(objects)
+
+    print("Frame extraction completed.")
