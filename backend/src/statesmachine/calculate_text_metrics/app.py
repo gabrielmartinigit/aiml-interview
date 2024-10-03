c161133afde65d066a291b90fcb33a4e99e5d5c4
gabrielcalmeida <gcouto@gmail.com>
2024-10-03 21:37:01 +0000

    chg llm model to claude-3-sonnet-20240229-v1:0

--- backend/src/statesmachine/calculate_text_metrics/app.py
+++ backend/src/statesmachine/calculate_text_metrics/app.py
@@ -10,7 +10,7 @@ from langchain.llms import Bedrock
 BUCKET = os.environ["BUCKET"]
 s3 = boto3.client("s3")
 
-LLM_MODEL_ID = "anthropic.claude-v2:1"
+LLM_MODEL_ID = "anthropic.claude-3-sonnet-20240229-v1:0"
 
 metrics = {
     "transcription": "",
@@ -19,43 +19,61 @@ metrics = {
 
 bedrock = boto3.client(
     "bedrock-runtime",
-    endpoint_url="https://bedrock-runtime.us-east-1.amazonaws.com",
     region_name="us-east-1",
 )
 
-# Ask LLM
-llm_mentor_response = Bedrock(
-    model_id=LLM_MODEL_ID,
-    model_kwargs={
-        "max_tokens_to_sample": 8000,
-        "temperature": 0.2,
-        "top_p": 0.7,
-    },
-    client=bedrock,
-)
+# # Ask LLM
+# llm_mentor_response = Bedrock(
+#     model_id=LLM_MODEL_ID,
+#     model_kwargs={
+#         "max_tokens_to_sample": 8000,
+#         "temperature": 0.2,
+#         "top_p": 0.7,
+#     },
+#     client=bedrock,
+# )
 
 
 def bedrock_feedback(perguntas, apresentacao):
-    prompt_template_bedrock = f"""
-    Human: Você é um professor. Avalie a simulação de entrevista do aluno e corrija as respostas das perguntas. A apresentação do cadidato é a seguinte:
-    
+    prompt_template_bedrock = f"""Use a transcrição da entrevista para auxiliar o entrevistador:
+  
     <perguntas>{perguntas}</perguntas>
     <apresentação>{apresentacao}</apresentação>
     
     A resposta deve seguir o seguinte formato:
     <avaliação>Avaliação geral e de boas práticas de apresentação</avaliação>
     <correção>Correção das respostas do aluno para as perguntas</correção>
-    
-    Assistant:
     """
-    chain = llm_mentor_response(prompt_template_bedrock)
-    return str(chain)
+    
+    response = bedrock.invoke_model(
+        modelId=LLM_MODEL_ID,
+        body=json.dumps(
+            {
+                "anthropic_version": "bedrock-2023-05-31",
+                "max_tokens": 2048,
+                "system": "Você é um entrevistador. Avalie a simulação de entrevista do aluno e corrija as respostas das perguntas, avaliando se estão corretas ou não para cada uma delas.",
+                "messages": [
+                    {
+                        "role": "user",
+                        "content": [{"type": "text", "text": prompt_template_bedrock}],
+                    }
+                ],
+                "temperature": 0.5,
+            }
+        ),
+    )
+
+    result = (
+        json.loads(response.get("body").read())
+        .get("content", [])[0]
+        .get("text", "")
+    )
+    
+    return result
 
 
 def lambda_handler(event, context):
-    transcription_file = event["TranscriptionJob"]["Transcript"][
-        "TranscriptFileUri"
-    ]
+    transcription_file = event["TranscriptionJob"]["Transcript"]["TranscriptFileUri"]
 
     key = os.path.splitext(os.path.basename(transcription_file))
     s3.download_file(
